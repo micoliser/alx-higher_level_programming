b3e61d35a52954e3be9c0fa7a87b41f4322e0fb2
micoliser <micoliser@gmail.com>
2023-01-08 19:22:51 +0100

    100-print_python_list_info.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,26 +8,30 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = *head, *temp2 = *head;
+	listint_t *temp = *head;
 	int length, *list_arr, i;
 
 	if (!(*head))
 		return (1);
 
-	for (length = 0; temp2; length++)
-		temp2 = temp2->next;
+	for (length = 0; temp; length++)
+		temp = temp->next;
 
 	list_arr = malloc(sizeof(int) * length);
 	if (!list_arr)
 		exit(100);
 
+	temp = *head;
 	for (i = 0; temp; temp = temp->next, i++)
 		list_arr[i] = temp->n;
 
 	for (i = 0; i < (length / 2); i++)
 	{
 		if (list_arr[i] != list_arr[length - 1 - i])
+		{
+			free(list_arr);
 			return (0);
+		}
 	}
 
 	free(list_arr);
