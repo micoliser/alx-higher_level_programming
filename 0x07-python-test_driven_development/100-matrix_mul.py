8c22ee8c9d387127de7eefb9d9d418232297f84b
micoliser <micoliser@gmail.com>
2023-01-30 11:37:48 +0100

    advanced

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -13,16 +13,19 @@ def matrix_mul(m_a, m_b):
     matrixes = [m_a, m_b]
     for i in range(2):
         if type(matrixes[i]) is not list:
-            raise TypeError("{} must be a list".format(msg[i]))
+            raise TypeError("{} must be a list"
+                            .format(msg[i]))
 
     for i in range(2):
         for li in matrixes[i]:
             if type(li) is not list:
-                raise TypeError("{} must be a list of lists".format(msg[i]))
+                raise TypeError("{} must be a list of lists"
+                                .format(msg[i]))
 
     for i in range(2):
         if len(matrixes[i]) == 0:
             raise ValueError("{} can't be empty".format(msg[i]))
+
         for li in matrixes[i]:
             if len(li) == 0:
                 raise ValueError("{} can't be empty".format(msg[i]))
@@ -31,14 +34,18 @@ def matrix_mul(m_a, m_b):
         for li in matrixes[i]:
             for elem in li:
                 if type(elem) is not int and type(elem) is not float:
-                    raise TypeError("{} should contain only integers or floats".format(msg[i]))
+                    raise TypeError("{} should contain only"
+                                    " integers or floats"
+                                    .format(msg[i]))
 
     for i in range(2):
         matrix = matrixes[i]
         row_size = len(matrix[0])
         for j in range(1, len(matrix)):
             if len(matrix[j]) != row_size:
-                raise TypeError("each row of {} must be of the same size".format(msg[i]))
+                raise TypeError("each row of {} must be "
+                                "of the same size"
+                                .format(msg[i]))
 
     m_a_col_size = len(m_a[0])
     m_b_row_size = len(m_b)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,12 +16,15 @@ def matrix_divided(matrix, div):
     for row in matrix:
         for data in row:
             if type(data) is not int and type(data) is not float:
-                raise TypeError("""matrix must be a matrix (list of lists) of integers/floats""")
+                raise TypeError("matrix must be a matrix "
+                                "(list of lists) of "
+                                "integers/floats")
 
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix "
+                            "must have the same size")
 
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
