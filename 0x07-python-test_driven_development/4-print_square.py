9215aa4c404fdfc6ee5b428076881784b44d54b2
micoliser <micoliser@gmail.com>
2023-02-01 09:46:40 +0100

    more fixes

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,7 +17,9 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size is not 0:
+    if size == 0:
+        print()
+    else:
         for i in range(size):
             for j in range(size):
                 print("#", end="")
