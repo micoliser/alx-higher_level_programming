e25341a0299623e43b0cac711b5af7fae8049b17
micoliser <micoliser@gmail.com>
2023-03-14 16:13:24 +0100

    advanced

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- displays the average temperatures by city ordered by temperature
+SELECT city, AVG(value) as avg_temp FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- displays the top 3 cities temperature during july and august
+SELECT city, AVG(value) as avg_temp FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
