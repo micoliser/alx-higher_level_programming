bac678e36d27d3f1c8cbaee7db85be32bb232f2e
micoliser <micoliser@gmail.com>
2023-03-14 15:25:24 +0100

    mandatory fix

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- lists all records of the table second table
 -- not listing rows without a name value
 -- sorted by descending score
-SELECT score, name from second_table
+SELECT score, name FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
