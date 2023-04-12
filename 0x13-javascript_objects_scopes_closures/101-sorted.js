8b8a95d22def9b8ffd1da88a754f463187e17871
micoliser <micoliser@gmail.com>
2023-04-13 00:39:43 +0100

    tasks done

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,6 +5,7 @@ const newDict = {};
 Object.keys(dict).map((key, index) => {
   if (newDict[dict[key]] === undefined) newDict[dict[key]] = [];
   newDict[dict[key]].push(key);
+  return index;
 });
 
 console.log(newDict);
